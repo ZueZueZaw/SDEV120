890f359f5a8bf570aaa2ab288c3cce790b3cfe1e
ZueZueZaw <zueeaintmiatchel@gmail.com>
2026-04-26 23:30:26 -0400

    Add files via upload

---
+++ module6_python program_ZueZueZaw.py
@@ -0,0 +1,18 @@
+"""
+Program: Raining Cats and Dogs
+Description:
+This program sets variables and prints:
+"It is raining cats and dogs."
+"""
+
+# Variable for cats
+c = "cats"
+
+# Variable for dogs
+d = "dogs."
+
+# Combine into full sentence
+s = "It is raining " + c + " and " + d
+
+# Output result
+print(s)
